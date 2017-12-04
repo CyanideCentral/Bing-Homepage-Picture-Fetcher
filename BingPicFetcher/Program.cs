4a9802709751acf896bb7fa851b48009afd55a77
CyanideCentral <tetracyanoethylene@163.com>
2017-12-04 12:01:40 +0800

    Initial commit Not yet implemented.

---
+++ BingPicFetcher/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace BingPicFetcher {
+    class Fetcher {
+        static String urlBase = "https://www.bing.com/HPImageArchive.aspx?format=json&idx=0&n=1&mkt=en-US";
+        static String resDefault = "_1366x768";
+        static String resSpec = "_1920x1080";
+        static void Main(string[] args) {
+            WebRequest request = WebRequest.Create(urlBase);
+
+        }
+    }
+}
